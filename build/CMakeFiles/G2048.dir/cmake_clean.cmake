79629146a829db525cdea4e2a799a91c435e1e69
wilsonshaw <xiaowenchaode@qq.com>
2022-01-11 00:53:34 +0800

    在更新了cmake之后，重新弄了一遍目录，现在调试正常，可以继续项目的编写

---
+++ build/CMakeFiles/G2048.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/G2048.dir/main.cpp.o"
+  "CMakeFiles/G2048.dir/main.cpp.o.d"
+  "G2048"
+  "G2048.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/G2048.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
